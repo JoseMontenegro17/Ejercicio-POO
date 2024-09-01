80ceafdc2e11720213e1e88dfde738228efffbb3
Alejandro Montenegro <u20241222690@usco.edu.co>
2024-09-01 10:59:28 -0500

    modificación 1

---
+++ ejercicioUno.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yoarc
+ */
+public class ejercicioUno {
+    
+    public static void main(String[] args) {
+         import java.util.Scanner
+       /* Crear un programa que pide un nombre por Scanner y escribe en consola "buenas tardes, Sr XX"(Evidentemente, sustituyendo XX por el nombre que escriba en cosola*/
+       
+               
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter username");
+        String Enter username = sc.nextLine();
+        System.out.println("Buenas tardes Sr" + Enter username);
+        
+    }
+       
+    }
+    
+    
+
