921389af2807d067f7c8b57582954ea3dadb7b0a
Alejandro Montenegro <u20241222690@usco.edu.co>
2024-09-11 12:20:18 -0500

    java

--- ejercicioDoce.java
+++ ejercicioDoce.java
@@ -8,6 +8,33 @@ package app;
  *
  * @author Alejo
  */
+/* Crear un programa que declare dos variables de tipo entero y pida al usuario por teclado un valor 
+para cada una de ellas. A continuación muestra un mensaje indicando si ambos valores son iguales*/
+import java.util.Scanner;
 public class ejercicioDoce {
+    public static void main(String[] args) {
+  int num1, num2;
+  
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Ingrese un número:");     
+  num1 =sc.nextInt();
+  System.out.println("Ingrese otro número:");
+  num2 =sc.nextInt();
+  
+  switch (Integer.compare(num1, num2)){
+      case 0:
+      System.out.println("Ambos números son iguales");
+      break;
+      case 1:
+          System.out.println("EL primer número es mayor que el segundo");
+      break;
+      case -1:
+          System.out.println("El segundo número es mayor que el primero;");
+       break;
+       
+      
+  }
+   
+    }
     
 }
