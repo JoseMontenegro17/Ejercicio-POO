29236978af04fafa9390ead52b3be2877bb7756f
Alejandro Montenegro <u20241222690@usco.edu.co>
2024-10-01 19:49:12 -0500

    Ejercicio1

---
+++ PromPrin.java
@@ -0,0 +1,18 @@
+package Ejercicio1;
+
+public class PromPrin {
+ public static void main(String[] args) {
+     Papel hoja1 = new Papel();
+     Papel hoja2 = new Papel();
+     hoja1.color = "Blanco";
+     hoja1.grosor = 80;
+     hoja2.color = "Amarillo";
+     hoja2.grosor = 100;
+     System.out.println("Hoja 1 - Color: " + hoja1.color + ", Grosor: " + hoja1.grosor + "g");
+     System.out.println("Hoja 2 - Color: " + hoja2.color + ", Grosor: " + hoja2.grosor + "g");
+            
+        
+        
+    }
+    
+}
