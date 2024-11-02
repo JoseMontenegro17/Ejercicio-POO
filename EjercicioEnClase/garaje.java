57ed8dc5a2ffe2491d5f7bd73eda339dd6c7dc64
Alejandro Montenegro <u20241222690@usco.edu.co>
2024-11-01 20:01:53 -0500

    modificado con exito

--- EjercicioEnClase/garaje.java
+++ EjercicioEnClase/garaje.java
@@ -7,10 +7,14 @@ public class garaje {
    private  int cilindradad;
    private  double impuestoCirculacion;
    private double cuotaMesGaraje;
+   public static final int CONSTANTE=100;
     public garaje(String marca, double precio, int cilindradad) {
         this.marca = marca;
         this.precio = precio;
         this.cilindradad = cilindradad;
+        this.cuotaMesGaraje = cuotaMesGaraje;
+        this.matricula =null;
+        this.impuestoCirculacion= CONSTANTE;
     }
     public String getMatricula() {
         return matricula;
@@ -40,20 +44,24 @@ public class garaje {
         return impuestoCirculacion;
     }
     public void setImpuestoCirculacion(double impuestoCirculacion) {
+       
         this.impuestoCirculacion = impuestoCirculacion;
     }
     public double getCuotaMesGaraje() {
         return cuotaMesGaraje;
     }
     public void setCuotaMesGaraje(double cuotaMesGaraje) {
+        if(cuotaMesGaraje<0){
+            
         this.cuotaMesGaraje = cuotaMesGaraje;
+            }else System.out.println("Error, no puedes indicar una cuota negativa");
     }
     public void calcularImpuestoCirculacion() {
         this.impuestoCirculacion = 0.02 * precio;
     }
 
     public boolean matricular(String matricula) {
-        if (matricula.length() == 7) {
+        if (matricula.length() == 6) {
             this.matricula = matricula;
             return true;
         }
@@ -61,11 +69,10 @@ public class garaje {
     }
     public void calcularInpuesto(){
         this.impuestoCirculacion= 002* precio;
-    }
-    class moto extends vehiculo{
-        private boolean tieneSidecar;
 
     }
+    }
     
     
-}
+    
+
---
+++ EjercicioEnClase/vehiculo.java
@@ -0,0 +1,17 @@
+package EjercicioEnClase;
+
+
+class vehiculo extends garaje{
+        public vehiculo(String marca, double precio, int cilindradad) {
+        super(marca, precio, cilindradad);
+        }
+
+        private boolean tieneSidecar;
+        this.tieneSidecar = tieneSidecar;
+        if (tieneSidecar==true){
+         
+        
+            
+        }
+    }
+
