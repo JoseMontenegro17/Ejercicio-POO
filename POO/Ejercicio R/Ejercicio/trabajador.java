638c0cb7e5e77bb43ae18fd705695d963961b6fa
Alejandro Montenegro <u20241222690@usco.edu.co>
2024-11-10 13:28:28 -0500

    .

--- POO/Ejercicio R/Ejercicio/trabajador.java
+++
@@ -1,5 +0,0 @@
-// Interfaz Trabajador
-public interface trabajador {
-    void trabajar(); // Método que define la actividad de trabajo
-    void reportar(); // Método para reportar las actividades
-}
